e5e4e13263bc5e4d749403cac479682899a0f641
Mirnes420 <genzen2021@gmail.com>
2023-06-26 11:44:23 +0200

    moved all the views into seperated folder

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../App.css';
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
